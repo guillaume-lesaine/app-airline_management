c1e568cd97ed31935dcf647df1970692cd9231e4
Guillaume LESAINE <guillaume.lesaine@gmail.com>
2018-10-22 19:00:23 +0200

    GCA 0.3
    - Possibilité d'entrer un nouveau vol
    - Création de custom validators remplaçant certains messages flash

--- application_gca/app/forms.py
+++ application_gca/app/forms.py
@@ -1,15 +1,53 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, StringField, DecimalField, PasswordField, BooleanField, SubmitField, SelectMultipleField, widgets
-from wtforms.validators import DataRequired, Optional
+from wtforms.validators import DataRequired, Optional, NumberRange, ValidationError
+
+
+def year_check(form, field):
+    if len(field.data) != 4:
+        raise ValidationError('Champ à 4 caractères !')
+
+def month_check(form,field):
+    x=0
+    try :
+        x=int(field.data)
+    except:
+        raise ValidationError('1 - 12 !')
+    if x<1 or x>12:
+        raise ValidationError('1 - 12 !')
+
+def day_check(form,field):
+    x=0
+    try :
+        x=int(field.data)
+    except:
+        raise ValidationError('Entrer un nombre !')
+    if x<1 or x>31:
+        raise ValidationError('1 - 31 !')
+
+def hour_check(form,field):
+    x=0
+    try :
+        x=int(field.data)
+    except:
+        raise ValidationError('Entrer un nombre !')
+    if x<0 or x>23:
+        raise ValidationError('0 - 23 !')
+
+def minute_check(form,field):
+    x=0
+    try :
+        x=int(field.data)
+    except:
+        raise ValidationError('Entrer un nombre !')
+    if x<0 or x>60:
+        raise ValidationError('0 - 60 !')
 
 class AirportCreationForm(FlaskForm):
     code = StringField('Code de l\'aéroport', validators=[DataRequired()])
     nom = StringField('Nom de l\'aéroport', validators=[DataRequired()])
     submit = SubmitField('Valider')
 
-class MultiCheckboxField(SelectMultipleField):
-    widget = widgets.ListWidget(prefix_label=False)
-    option_widget = widgets.CheckboxInput()
 
 class EmployeeCreationForm(FlaskForm):
     numero_securite_sociale=IntegerField('Numéro de sécurité sociale', validators=[DataRequired()])
@@ -25,9 +63,24 @@ class EmployeeCreationForm(FlaskForm):
 class NaviguantCreationForm(FlaskForm):
     nbr_heures_vol = IntegerField('Nombre d\'heures de vol', validators=[DataRequired()])
     fonction = SelectMultipleField('Type',choices=[('pilote', 'Pilote'), ('steward', 'Steward'), ('hotesse', 'Hôtesse')],validators=[DataRequired()])
-    num_license_pilote = IntegerField('Numéro de license',validators=[Optional()])
+    num_licence_pilote = IntegerField('Numéro de licence',validators=[Optional()])
     submit = SubmitField('Valider')
-#
+
+class VolCreationForm(FlaskForm):
+    num_vol = StringField('Nombre d\'heures de vol', validators=[DataRequired()])
+    ts_annee_depart = StringField('Année', validators=[DataRequired(),year_check])
+    ts_mois_depart = StringField('Mois', validators=[DataRequired(),month_check])
+    ts_jour_depart = StringField('Jour', validators=[DataRequired(),day_check])
+    ts_heure_depart = StringField('Heure', validators=[DataRequired(),hour_check])
+    ts_minute_depart = StringField('Minute', validators=[DataRequired(),minute_check])
+    ts_annee_arrivee = StringField('Année', validators=[DataRequired(),year_check])
+    ts_mois_arrivee = StringField('Mois', validators=[DataRequired(),month_check])
+    ts_jour_arrivee = StringField('Jour', validators=[DataRequired(),day_check])
+    ts_heure_arrivee = StringField('Heure', validators=[DataRequired(),hour_check])
+    ts_minute_arrivee = StringField('Minute', validators=[DataRequired(),minute_check])
+    immatriculation_appareil = StringField('Immatriculation appareil', validators=[DataRequired()])
+    submit = SubmitField('Valider')
+
 # class SimpleForm(Form):
 #     string_of_files = ['one\r\ntwo\r\nthree\r\n']
 #     list_of_files = string_of_files[0].split()
--- application_gca/app/routes.py
+++ application_gca/app/routes.py
@@ -1,6 +1,6 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
-from app.forms import AirportCreationForm, EmployeeCreationForm, NaviguantCreationForm
+from app.forms import AirportCreationForm, EmployeeCreationForm, NaviguantCreationForm, VolCreationForm
 from flask_mysqldb import MySQL
 
 mysql=MySQL(app)
@@ -9,24 +9,15 @@ mysql=MySQL(app)
 @app.route('/')
 @app.route('/index')
 def index():
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM aeroports")
-    #cur.execute("SELECT * FROM Stock")
-    data = cur.fetchall()
-    cur.close()
-    print(data)
     return render_template('index.html', title='Home')
 
 @app.route('/creation/airport', methods=['GET', 'POST'])
 def creation_airport():
     form = AirportCreationForm()
     if form.validate_on_submit():
+        cur = mysql.connection.cursor()
         code = form.code.data
         nom = form.nom.data
-        cur = mysql.connection.cursor()
-        print(nom)
-        print(code)
-        #cur.execute("SELECT * FROM aeroports")
         cur.execute("INSERT INTO aeroports(id_aeroports,code,nom) VALUES (DEFAULT, %s, %s)",(code,nom))
         mysql.connection.commit()
         cur.close()
@@ -37,8 +28,8 @@ def creation_airport():
 def creation_employee():
     form = EmployeeCreationForm()
     if form.validate_on_submit():
-        numero_securite_sociale = form.numero_securite_sociale.data
         cur = mysql.connection.cursor()
+        numero_securite_sociale = form.numero_securite_sociale.data
         cur.execute("SELECT numero_securite_sociale FROM employes")
         data = [x[0] for x in cur.fetchall()]
         if numero_securite_sociale in data:
@@ -56,27 +47,79 @@ def creation_employee():
             mysql.connection.commit()
             cur.close()
             if type == 'naviguant':
-                return redirect('/creation/employee/naviguant')
+                return redirect(url_for('creation_employee_naviguant',numero_securite_sociale = numero_securite_sociale))
             else:
                 return redirect('/index')
     return render_template('creation_employee.html', title='Création employé', form=form)
 
-@app.route('/creation/employee/naviguant', methods=['GET', 'POST'])
-def creation_employee_naviguant():
+@app.route('/creation/employee/naviguant/<numero_securite_sociale>', methods=['GET', 'POST'])
+def creation_employee_naviguant(numero_securite_sociale):
     form = NaviguantCreationForm()
     if form.validate_on_submit():
+        cur = mysql.connection.cursor()
         nbr_heures_vol = form.nbr_heures_vol.data
-        print(nbr_heures_vol)
         fonction = form.fonction.data[0]
-        print(fonction)
-        num_license_pilote = form.num_license_pilote.data
-        print(num_license_pilote)
-        if fonction == 'pilote' and num_license_pilote == None:
+        num_licence_pilote = form.num_licence_pilote.data
+        cur.execute("SELECT num_licence_pilote FROM naviguants")
+        data = [x[0] for x in cur.fetchall()]
+        if fonction == 'pilote' and num_licence_pilote == None:
             flash('Un pilote doit avoir un numéro de license')
         else :
-            flash('Un employé naviguant a été créé.')
-            #cur.execute("INSERT INTO employes(numero_securite_sociale,nom,prenom,adresse,ville,pays,salaire,type) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",(numero_securite_sociale,nom,prenom,adresse,ville,pays,salaire,type))
-            #mysql.connection.commit()
-            #cur.close()
-            return redirect('/index')
+            if num_licence_pilote in data:
+                flash('Le numéro de licence pilote {} existe déjà dans la base'.format(num_licence_pilote))
+            else:
+                flash('Un employé naviguant a été créé.')
+                cur.execute("INSERT INTO naviguants(numero_securite_sociale,nbr_heures_vol,fonction,num_licence_pilote) VALUES (%s, %s, %s, %s)",(numero_securite_sociale,nbr_heures_vol,fonction,num_licence_pilote))
+                mysql.connection.commit()
+                cur.close()
+                return redirect('/index')
     return render_template('creation_employee_naviguant.html', title='Création employé naviguant', form=form)
+
+@app.route('/creation/vol', methods=['GET', 'POST'])
+def creation_vol():
+    choix=[('pilote', 'Pilote'), ('steward', 'Steward'), ('hotesse', 'Hôtesse')]
+    form = VolCreationForm()
+    if form.validate_on_submit():
+        to_flash=[]
+        num_vol = form.num_vol.data
+        ts_annee_depart = form.ts_annee_depart.data
+        ts_mois_depart = form.ts_mois_depart.data
+        ts_jour_depart = form.ts_jour_depart.data
+        ts_heure_depart = form.ts_heure_depart.data
+        ts_minute_depart = form.ts_minute_depart.data
+        ts_annee_arrivee = form.ts_annee_arrivee.data
+        ts_mois_arrivee = form.ts_mois_arrivee.data
+        ts_jour_arrivee = form.ts_jour_arrivee.data
+        ts_heure_arrivee = form.ts_heure_arrivee.data
+        ts_minute_arrivee = form.ts_minute_arrivee.data
+        immatriculation_appareil = form.immatriculation_appareil.data
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT num_immatriculation FROM appareils")
+        data = [x[0] for x in cur.fetchall()]
+        if immatriculation_appareil not in data:
+            flash('Le numéro d\'immatriculation {} n\'existe pas dans la base de données. Veuillez rentrer un numéro valide'.format(immatriculation_appareil))
+        else :
+            ts_depart=ts_annee_depart+'-'+ts_mois_depart+'-'+ts_jour_depart+' '+ts_heure_depart+':'+ts_minute_depart+':00'
+            ts_arrivee=ts_annee_arrivee+'-'+ts_mois_arrivee+'-'+ts_jour_arrivee+' '+ts_heure_arrivee+':'+ts_minute_arrivee+':00'
+            cur.execute("INSERT INTO vols(num_vol,ts_depart,ts_arrivee,immatriculation_appareil) VALUES (%s, %s, %s, (SELECT num_immatriculation from appareils where num_immatriculation = %s))",(num_vol,ts_depart,ts_arrivee,immatriculation_appareil))
+            mysql.connection.commit()
+            cur.close()
+            return redirect('/index')
+    #     cur = mysql.connection.cursor()
+    #     nbr_heures_vol = form.nbr_heures_vol.data
+    #     fonction = form.fonction.data[0]
+    #     num_licence_pilote = form.num_licence_pilote.data
+    #     cur.execute("SELECT num_licence_pilote FROM naviguants")
+    #     data = [x[0] for x in cur.fetchall()]
+    #     if fonction == 'pilote' and num_licence_pilote == None:
+    #         flash('Un pilote doit avoir un numéro de license')
+    #     else :
+    #         if num_licence_pilote in data:
+    #             flash('Le numéro de licence pilote {} existe déjà dans la base'.format(num_licence_pilote))
+    #         else:
+    #             flash('Un employé naviguant a été créé.')
+    #             cur.execute("INSERT INTO naviguants(numero_securite_sociale,nbr_heures_vol,fonction,num_licence_pilote) VALUES (%s, %s, %s, %s)",(numero_securite_sociale,nbr_heures_vol,fonction,num_licence_pilote))
+    #             mysql.connection.commit()
+    #             cur.close()
+    #             return redirect('/index')
+    return render_template('creation_vol.html', title='Création vol', form=form)
