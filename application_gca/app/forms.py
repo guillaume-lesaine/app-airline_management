b977e04da050f9f80d5b847587ff8a1b566decfe
Guillaume LESAINE <guillaume.lesaine@gmail.com>
2018-10-22 13:40:24 +0200

    GCA 0.2
    - Connection base de données
    - Formulaire création employé (naviguant)
    - Messages flash informant l'utilisateur.

--- application_gca/app/forms.py
+++ application_gca/app/forms.py
@@ -1,8 +1,36 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import IntegerField, StringField, DecimalField, PasswordField, BooleanField, SubmitField, SelectMultipleField, widgets
+from wtforms.validators import DataRequired, Optional
 
 class AirportCreationForm(FlaskForm):
-    name = StringField('Nom de l\'aéroport', validators=[DataRequired()])
     code = StringField('Code de l\'aéroport', validators=[DataRequired()])
+    nom = StringField('Nom de l\'aéroport', validators=[DataRequired()])
     submit = SubmitField('Valider')
+
+class MultiCheckboxField(SelectMultipleField):
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
+
+class EmployeeCreationForm(FlaskForm):
+    numero_securite_sociale=IntegerField('Numéro de sécurité sociale', validators=[DataRequired()])
+    nom = StringField('Nom', validators=[DataRequired()])
+    prenom = StringField('Prénom', validators=[DataRequired()])
+    adresse = StringField('Adresse', validators=[DataRequired()])
+    ville = StringField('Ville', validators=[DataRequired()])
+    pays = StringField('Pays', validators=[DataRequired()])
+    salaire = DecimalField(places=2, validators=[DataRequired()])
+    type = SelectMultipleField('Type',choices=[('naviguant', 'Naviguant'), ('au_sol', 'Au sol')],validators=[DataRequired()])
+    submit = SubmitField('Valider')
+
+class NaviguantCreationForm(FlaskForm):
+    nbr_heures_vol = IntegerField('Nombre d\'heures de vol', validators=[DataRequired()])
+    fonction = SelectMultipleField('Type',choices=[('pilote', 'Pilote'), ('steward', 'Steward'), ('hotesse', 'Hôtesse')],validators=[DataRequired()])
+    num_license_pilote = IntegerField('Numéro de license',validators=[Optional()])
+    submit = SubmitField('Valider')
+#
+# class SimpleForm(Form):
+#     string_of_files = ['one\r\ntwo\r\nthree\r\n']
+#     list_of_files = string_of_files[0].split()
+#     # create a list of value/description tuples
+#     files = [(x, x) for x in list_of_files]
+#     example = MultiCheckboxField('Label', choices=files)
--- application_gca/app/routes.py
+++ application_gca/app/routes.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
-from app.forms import AirportCreationForm
+from app.forms import AirportCreationForm, EmployeeCreationForm, NaviguantCreationForm
 from flask_mysqldb import MySQL
 
 mysql=MySQL(app)
@@ -10,8 +10,9 @@ mysql=MySQL(app)
 @app.route('/index')
 def index():
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM Stock")
-    data = cur.fetchone()
+    cur.execute("SELECT * FROM aeroports")
+    #cur.execute("SELECT * FROM Stock")
+    data = cur.fetchall()
     cur.close()
     print(data)
     return render_template('index.html', title='Home')
@@ -20,11 +21,62 @@ def index():
 def creation_airport():
     form = AirportCreationForm()
     if form.validate_on_submit():
-        name = form.name.data
         code = form.code.data
+        nom = form.nom.data
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO Stock(idStock,idIngredient,quantite) VALUES(%s, %s, %s)",(10,name,code))
+        print(nom)
+        print(code)
+        #cur.execute("SELECT * FROM aeroports")
+        cur.execute("INSERT INTO aeroports(id_aeroports,code,nom) VALUES (DEFAULT, %s, %s)",(code,nom))
         mysql.connection.commit()
         cur.close()
         return redirect('/index')
     return render_template('creation_airport.html', title='Création aéroport', form=form)
+
+@app.route('/creation/employee', methods=['GET', 'POST'])
+def creation_employee():
+    form = EmployeeCreationForm()
+    if form.validate_on_submit():
+        numero_securite_sociale = form.numero_securite_sociale.data
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT numero_securite_sociale FROM employes")
+        data = [x[0] for x in cur.fetchall()]
+        if numero_securite_sociale in data:
+            flash('Le numéro de sécurité sociale {} existe déjà dans la base'.format(numero_securite_sociale))
+        else :
+            nom = form.nom.data
+            prenom = form.prenom.data
+            adresse = form.adresse.data
+            ville = form.ville.data
+            pays = form.pays.data
+            salaire = form.salaire.data
+            type = form.type.data[0]
+            flash('{} {} est désormais un employé.'.format(prenom,nom))
+            cur.execute("INSERT INTO employes(numero_securite_sociale,nom,prenom,adresse,ville,pays,salaire,type) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",(numero_securite_sociale,nom,prenom,adresse,ville,pays,salaire,type))
+            mysql.connection.commit()
+            cur.close()
+            if type == 'naviguant':
+                return redirect('/creation/employee/naviguant')
+            else:
+                return redirect('/index')
+    return render_template('creation_employee.html', title='Création employé', form=form)
+
+@app.route('/creation/employee/naviguant', methods=['GET', 'POST'])
+def creation_employee_naviguant():
+    form = NaviguantCreationForm()
+    if form.validate_on_submit():
+        nbr_heures_vol = form.nbr_heures_vol.data
+        print(nbr_heures_vol)
+        fonction = form.fonction.data[0]
+        print(fonction)
+        num_license_pilote = form.num_license_pilote.data
+        print(num_license_pilote)
+        if fonction == 'pilote' and num_license_pilote == None:
+            flash('Un pilote doit avoir un numéro de license')
+        else :
+            flash('Un employé naviguant a été créé.')
+            #cur.execute("INSERT INTO employes(numero_securite_sociale,nom,prenom,adresse,ville,pays,salaire,type) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",(numero_securite_sociale,nom,prenom,adresse,ville,pays,salaire,type))
+            #mysql.connection.commit()
+            #cur.close()
+            return redirect('/index')
+    return render_template('creation_employee_naviguant.html', title='Création employé naviguant', form=form)
