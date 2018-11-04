2b3b76ccb389ad63ca554281e561e85934cde55a
Guillaume LESAINE <guillaume.lesaine@gmail.com>
2018-11-04 14:55:06 +0100

    Database information

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
     #Configure DB
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = ''
-    MYSQL_DB = 'airline'
+    MYSQL_HOST = 'us-cdbr-iron-east-01.cleardb.net'
+    MYSQL_USER = 'bee711030d7cd2'
+    MYSQL_PASSWORD = 'd47e8ba9'
+    MYSQL_DB = 'heroku_03b85268317665c'
