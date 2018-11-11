364729dd60b54da1a77864f06aa65ef8b3df9ff7
Guillaume LESAINE <guillaume.lesaine@gmail.com>
2018-11-11 11:04:46 +0100

    DB

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
-    Configure DB
+    #Configure DB
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
     MYSQL_PASSWORD = 
